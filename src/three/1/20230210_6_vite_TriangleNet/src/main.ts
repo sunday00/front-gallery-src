46f64b47a3c009e9282d0acb789213df6f330fef
sunday00tdi <102128587+sunday00tdi@users.noreply.github.com>
2023-02-14 01:09:30 +0900

    :construction: raycaster
    raycaster

--- src/three/1/20230210_6_vite_TriangleNet/src/main.ts
+++ src/three/1/20230210_6_vite_TriangleNet/src/main.ts
@@ -1,14 +1,31 @@
 import './style.scss'
 import * as three from 'three'
 import {Pane} from "tweakpane";
-import {Mesh, MeshPhongMaterial, PerspectiveCamera, PlaneGeometry, Scene, WebGLRenderer} from "three";
+import {
+  BufferAttribute, Intersection,
+  Mesh,
+  MeshPhongMaterial,
+  PerspectiveCamera,
+  PlaneGeometry,
+  Raycaster,
+  Scene,
+  WebGLRenderer
+} from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import gsap from 'gsap'
+
+type LengthArray<T, N extends number, R extends T[] = []>
+  = number extends N ? T[] :
+    R['length'] extends N ? R
+    : LengthArray<T, N, [T, ...R]>;
 
 class Package {
   private scene!: Scene;
   private camera!: PerspectiveCamera;
   private renderer!: WebGLRenderer;
   private mesh!: Mesh<PlaneGeometry, MeshPhongMaterial>;
+  private raycaster!: Raycaster;
+  private mouse: {x: undefined|number, y: undefined|number} = {x: undefined, y: undefined};
 
   init() {
     this.scene = new three.Scene()
@@ -19,11 +36,12 @@ class Package {
     this.renderer.setSize(window.innerWidth, window.innerHeight)
     this.renderer.setPixelRatio(window.devicePixelRatio)
 
-    const geometry = new three.PlaneGeometry(5, 5, 10, 10)
+    const geometry = new three.PlaneGeometry(17, 11, 17, 11)
     const material = new three.MeshPhongMaterial({
-      color: '#0000FF',
+      // color: '#0000FF',
       side: three.DoubleSide,
-      flatShading: true
+      flatShading: true,
+      vertexColors: true,
     })
     this.mesh = new three.Mesh(geometry, material)
 
@@ -47,14 +65,66 @@ class Package {
 
     this.gui()
 
+    this.raycaster = new three.Raycaster()
+
     const animation = () => {
       requestAnimationFrame(animation)
       this.renderer.render(this.scene, this.camera)
+
+      this.raycaster.setFromCamera(
+        this.mouse as {x: number, y:number},
+        this.camera
+      )
+      const intersects = this.raycaster.intersectObject(this.mesh)
+
+      if(intersects.length) {
+        // @ts-ignore
+        const geometry = intersects[0].object.geometry as PlaneGeometry
+        const {color} = geometry.attributes
+
+        const initialColors = {
+          r: color.array[0],
+          g: color.array[1],
+          b: color.array[2],
+        }
+
+        const hoverColors = {
+          r: this.safelyBrighter(initialColors.r),
+          g: this.safelyBrighter(initialColors.g),
+          b: this.safelyBrighter(initialColors.b),
+        }
+
+        this.updateColor(color as BufferAttribute, intersects, hoverColors)
+
+        gsap.to(hoverColors, {
+          r: initialColors.r,
+          g: initialColors.g,
+          b: initialColors.b,
+          onUpdate: () => {
+            this.updateColor(color as BufferAttribute, intersects, hoverColors)
+          }
+        })
+      }
     }
 
     animation()
 
     this.mouseEvent()
+
+    const updateRenderer = () => {
+      this.renderer.setSize(
+        window.innerWidth, window.innerHeight
+      )
+
+      this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    }
+
+    window.addEventListener('resize', () => {
+      updateRenderer()
+
+      this.camera.aspect = window.innerWidth / window.innerHeight
+      this.camera.updateProjectionMatrix()
+    })
 
     document.body.appendChild(this.renderer.domElement)
   }
@@ -75,6 +145,7 @@ class Package {
 
   private makePlaneSegmentsUnFlatten() {
     const polygons = this.mesh.geometry.attributes.position.array
+    const colors = []
 
     for (let i=0; i<polygons.length;i+=3){
       // const x = polygons[i]
@@ -83,7 +154,14 @@ class Package {
 
       // @ts-ignore
       polygons[i + 2] = z + Math.random()
+
+      colors.push(...this.convert255ArrayTo1Array([10, 65, 105]))
     }
+
+    this.mesh.geometry.setAttribute(
+      'color',
+      new three.BufferAttribute(new Float32Array(colors), 3)
+    )
   }
 
   private planeSegmentsSetBumpy(bumpySize = 1) {
@@ -96,8 +174,42 @@ class Package {
   }
 
   private mouseEvent() {
-    const mouse = { x: undefined,  y: undefined}
+    window.addEventListener('mousemove', (e) => {
+      this.mouse.x = (e.clientX / window.innerWidth) * 2 - 1
+      this.mouse.y = - (e.clientY / window.innerHeight) * 2 + 1
+    })
+  }
 
+  private convert255to1(ten: number): number {
+    return Math.floor(ten / 255 * 10000) / 10000
+  }
+
+  private convert255ArrayTo1Array(tens: LengthArray<number, 3>): LengthArray<number, 3> {
+    return [
+      this.convert255to1(tens[0]),
+      this.convert255to1(tens[1]),
+      this.convert255to1(tens[2]),
+    ]
+  }
+
+  private safelyBrighter(r: number): number {
+    let v = r * 1.8
+    if(v > 1) v = 1
+
+    return v
+  }
+
+  private updateColor(
+    color: BufferAttribute,
+    intersects: Array<Intersection>,
+    hoverColors: { r: number; b: number; g: number }
+  ) {
+    const {r, g, b} = hoverColors
+
+    color.setXYZ(intersects[0].face!.a, r, g, b)
+    color.setXYZ(intersects[0].face!.b, r, g, b)
+    color.setXYZ(intersects[0].face!.c, r, g, b)
+    color.needsUpdate = true
   }
 
   private gui() {
@@ -120,7 +232,7 @@ class Package {
 
     const planeSizeSetting = {
       min: 0,
-      max: 10,
+      max: 20,
       step: 0.01,
     }
     const planeSegmentsSetting = {
@@ -159,6 +271,7 @@ class Package {
 
     const planeBumpySize = { value: 1 }
     const planeBumpySizeSetting = {
+      label: 'bumpSize',
       min: 0,
       max: 3,
       step: 0.1,
